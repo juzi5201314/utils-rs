36476dcd15c4741ea3f571cc8a00d11ad84257a1
soeur <juzi201314@gmail.com>
2021-08-03 17:57:45 +0800

    builder! macro support generics

--- src/builder.rs
+++ src/builder.rs
@@ -1,39 +1,37 @@
 #[macro_export]
 macro_rules! builder {
-    ($(#[$attr:meta])* $vis:vis $name:ident { $($(#[$field_attr:meta])* $field_name:ident: $field_type:ty $(=> { $(#[$setter_attr:meta])* $($t:tt)* })?),* }) => {
-        $(#[$attr])* $vis struct $name {
+    ($(#[$attr:meta])* $vis:vis $name:ident$(<$($g:ident),*>)? { $($(#[$field_attr:meta])* $field_name:ident: $field_type:ty $(=> { $(#[$setter_attr:meta])* $($t:tt)* })?),* }) => {
+        $(#[$attr])* $vis struct $name$(<$($g),*>)? {
             $($(#[$field_attr])* $field_name: $field_type),*
         }
 
-        impl $name {
+        impl$(<$($g),*>)? $name$(<$($g),*>)? {
             #[allow(dead_code)]
             pub fn new() -> Self {
                 Default::default()
             }
         }
 
-        $($crate::builder_setter!{$name, $field_name, $field_type $(=> { $(#[$setter_attr])* $($t)* })? })*
+        impl$(<$($g),*>)? $name$(<$($g),*>)? {
+            $($crate::builder_setter!{$field_name, $field_type $(=> { $(#[$setter_attr])* $($t)* })? })*
+        }
     };
 }
 
 #[macro_export]
 macro_rules! builder_setter {
-    ($name:ident, $field_name:ident, $field_type:ty => { $(#[$setter_attr:meta])* $($t:tt)* }) => {
-        impl $name {
-            #[inline]
-            #[allow(dead_code)]
-            $(#[$setter_attr:meta])*
-            pub fn $($t)*
-        }
+    ($field_name:ident, $field_type:ty => { $(#[$setter_attr:meta])* $($t:tt)* }) => {
+        #[inline]
+        #[allow(dead_code)]
+        $(#[$setter_attr:meta])*
+        pub fn $($t)*
     };
-    ($name:ident, $field_name:ident, $field_type:ty) => {
-        impl $name {
-            #[inline]
-            #[allow(dead_code)]
-            pub fn $field_name(mut self, t: $field_type) -> Self {
-                self.$field_name = t;
-                self
-            }
+    ($field_name:ident, $field_type:ty) => {
+        #[inline]
+        #[allow(dead_code)]
+        pub fn $field_name(mut self, t: $field_type) -> Self {
+            self.$field_name = t;
+            self
         }
     };
 }
